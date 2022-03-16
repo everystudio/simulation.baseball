a50d475afd4d49b692a604e1e8ef609b31ada4a5
watanabe@marida <watanabe.everystudio@gmail.com>
2022-03-16 14:39:48 +0900

    打席ごとに処理を行う準備

---
+++ Assets/Scripts/Daseki.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Daseki : MonoBehaviour
+{
+
+    public void OnDasekiResult()
+    {
+
+    }
+}
